51a2b09056bf1ce61b447def5a637cdba35e020f
victoronyonkaoroo <victor.oroo@student.moringaschool.com>
2023-06-22 21:59:05 +0300

    done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const mainElement = document.getElementById('main');
+if (mainElement) {
+  mainElement.parentNode.removeChild(mainElement);
+}
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'VICTOR is the champion';
